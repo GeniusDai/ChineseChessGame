c52a205b30e88abca9eafa969696290a43674e79
hao <dev@localhost.localdomain>
2021-11-25 20:07:38 +0800

    redirect to kingpin

--- src/StartServer.cc
+++
@@ -1,9 +0,0 @@
-#include "RAMIOHandler.h"
-#include "EpollTPServer.h"
-
-int main(int argc, char **argv) {
-    SharedData data;
-    EpollTPServer<RAMIOHandler, SharedData > server{8, 8889, &data};
-    server.start();
-    return 0;
-}
